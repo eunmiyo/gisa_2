de964d4c42a75728b6b3f771440aafad90b324c4
eunmiyo <good3103@gmail.com>
2022-12-23 17:54:35 +0900

    up

---
+++ cdp_1234/src/main/java/DTO/Vaccine.java
@@ -0,0 +1,48 @@
+package DTO;
+
+public class Vaccine {
+	String resvno;
+	String jumin;
+	String vcode;
+	String hcode;
+	String resvdate;
+	String resvtime;
+	
+	public String getResvno() {
+		return resvno;
+	}
+	public void setResvno(String resvno) {
+		this.resvno = resvno;
+	}
+	public String getJumin() {
+		return jumin;
+	}
+	public void setJumin(String jumin) {
+		this.jumin = jumin;
+	}
+	public String getVcode() {
+		return vcode;
+	}
+	public void setVcode(String vcode) {
+		this.vcode = vcode;
+	}
+	public String getHcode() {
+		return hcode;
+	}
+	public void setHcode(String hcode) {
+		this.hcode = hcode;
+	}
+	public String getResvdate() {
+		return resvdate;
+	}
+	public void setResvdate(String resvdate) {
+		this.resvdate = resvdate;
+	}
+	public String getResvtime() {
+		return resvtime;
+	}
+	public void setResvtime(String resvtime) {
+		this.resvtime = resvtime;
+	}
+	
+}
